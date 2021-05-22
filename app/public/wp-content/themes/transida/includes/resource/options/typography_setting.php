91a6a3a762efb79c65a1420eec18e51b5db13504
Darielle Davis <hello@darielledavis.com>
2021-05-22 18:51:17 -0400

    whoops... haven't pushed in forever

--- app/public/wp-content/themes/transida/includes/resource/options/typography_setting.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return array(
-
-    'title'         => esc_html__( 'Typography Settings', 'transida' ),
-    'id'            => 'typography_setting',
-    'desc'          => '',
-    'icon'          => 'el el-edit'
-);
